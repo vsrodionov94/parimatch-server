05fb7632f1e79722f02089be240db245a23925d8
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-12-15 17:52:23 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const { PORT, NODE_ENV, DB_LINK } = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const dbLink = NODE_ENV === 'production' ? DB_LINK : 'mongodb://localhost:27017/parimatch';
+mongoose.connect(dbLink, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+
+app.listen(NODE_ENV === 'production' ? PORT : 3000, () => {
+  console.log('123')
+});
