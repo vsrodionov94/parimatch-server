10fb35a13e84ab51b8f028678cf17a422f105c1a
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-12-16 17:24:14 +0700

    fix app js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const routes = require('./routes/index');
 const migration = require('./functions/migration');
 require('dotenv').config();
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_LINK } = process.env;
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const dbLink = 'mongodb://localhost:27017/parimatch';
+const dbLink = NODE_ENV === 'production' ? DB_LINK : 'mongodb://localhost:27017/parimatch';
 mongoose.connect(dbLink, {
   useNewUrlParser: true,
   useCreateIndex: true,
