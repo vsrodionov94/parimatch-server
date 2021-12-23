5230a554c8687f5b26907732243086e2345f6345
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-12-23 17:21:20 +0700

    add new changes

---
+++ routes/getAttachment.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+const sharp = require('sharp');
+const axios = require('axios');
+const FormData = require('form-data');
+const User = require('../models/user');
+
+const getImagesArray = array => {
+  const images = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] && array[i].correctly) {
+      images.push({ input: fs.readFileSync(`../images/${i}.png`) });
+    }
+  }
+  return images;
+};
+
+const compositeImage = async questions => {
+  const base = fs.readFileSync('../images/base.png');
+  const images = getImagesArray(questions);
+  try {
+    return await sharp(base)
+      .composite(images)
+      .toBuffer();
+  } catch (error) {
+    console.log(error);
+  }
+  return '';
+};
+
+const getUrl = async token => {
+  const method = 'photos.photos.getUploadServer';
+  const GROUP_ID = 163544497;
+  const ALBUM_ID = 251798458;
+  const ver = '5.131';
+
+  const response = await axios.get(`https://api.vk.com/method/${method}?access_token=${token}&group_id=${GROUP_ID}&album_id=${ALBUM_ID}&v=${ver}`);
+  return response.data.response.upload_url;
+};
+
+const loadImage = async (url, image) => {
+  const formData = new FormData();
+  formData.append('photo', image, 'image.png');
+
+  const response = await axios.post(url, formData, {
+    headers: {
+      'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`,
+    },
+  });
+  return response.data;
+};
+
+const saveImage = async (data, token) => {
+  const method = 'photos.saveMessagesPhoto';
+  const ver = '5.131';
+  const {
+    aid,
+    server,
+    photo,
+    hash,
+  } = data;
+  const response = await axios.get(`https://api.vk.com/method/${method}?access_token=${token}&photo=${photo}&aid=${aid}&server=${server}&hash=${hash}&v=${ver}`);
+  return response;
+};
+
+module.exports = app => {
+  app.post('/getAttachment', async (req, res) => {
+    const result = { error: false, attachment: '' };
+
+    const { vkId, token } = req.body;
+
+    const user = User.findOne({ vkId }).then(found => found);
+    if (user) {
+      const url = await getUrl(token);
+      if (url) {
+        const image = await compositeImage();
+        if (image) {
+          const response = await loadImage(url, image);
+          if (response) {
+            const attachment = await saveImage(response, token);
+            if (attachment) {
+              const { album_id: albumId, id } = attachment;
+              result.attachment = `photo-${albumId}_${id}`;
+            } else result.error = true;
+          } else result.error = true;
+        } else result.error = true;
+      } else result.error = true;
+    } else result.error = true;
+
+    res.json(result);
+  });
+};
