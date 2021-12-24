d17bd605823ad4d95cd4e262a35bc719481ef7c1
Vladimir Rodionov <vsrodionov94@yandex.ru>
2021-12-24 09:54:21 +0700

    fix

--- routes/sendRepost.js
+++ routes/sendRepost.js
@@ -4,6 +4,7 @@ const User = require('../models/user');
 module.exports = app => {
   app.post('/sendRepost', (req, res) => {
     const { vkId, repostId } = req.body;
+    console.log(req.body);
     User.updateOne({ vkId }, { $set: { lastRepost: repostId } }).then(() => null);
     Statistics.incRepostsCount();
     res.json({ error: false });
